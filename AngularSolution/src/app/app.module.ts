060c198c796a2598033e464db05bbb88ea30a814
jacksonrajesh <muduli.jackson@gmail.com>
2022-01-18 01:49:46 +0530

    New Com

--- AngularSolution/src/app/app.module.ts
+++ AngularSolution/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormComponent
+    ReactiveFormComponent,
+    TemplateDrivenComponent
   ],
   imports: [
     BrowserModule,
