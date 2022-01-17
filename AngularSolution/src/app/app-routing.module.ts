1b17b86d5c7ff03ad559b1e59caa7e195cb73fe4
jacksonrajesh <muduli.jackson@gmail.com>
2022-01-18 01:39:34 +0530

    adding component

--- AngularSolution/src/app/app-routing.module.ts
+++ AngularSolution/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:"reactiveform", component:ReactiveFormComponent,}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
