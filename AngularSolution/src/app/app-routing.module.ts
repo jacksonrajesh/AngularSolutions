503c0fe9cc4b165e7baaa5b9eefb3eadf9ca7b71
jacksonrajesh <muduli.jackson@gmail.com>
2022-01-21 02:32:22 +0530

    added forns and references

--- AngularSolution/src/app/app-routing.module.ts
+++ AngularSolution/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 
 const routes: Routes = [
-{path:"reactiveform", component:ReactiveFormComponent,}
-
+{path:"reactiveform", component:ReactiveFormComponent},
+{path:"templatedriven", component:TemplateDrivenComponent}
 ];
 
 @NgModule({
